9ecc61c041bea7847e1efa914b6815ef67688d72
Keith Fenske <github.kwfenske@gmail.com>
2021-03-03 10:12:56 -0700

    Add files via upload

--- CharMap4.java
+++ CharMap4.java
@@ -286,7 +286,7 @@ public class CharMap4
       word = args[i].toLowerCase(); // easier to process if consistent case
       if (word.length() == 0)
       {
-        /* Ignore null parameters, which are more common that you might think,
+        /* Ignore empty parameters, which are more common than you might think,
         when programs are being run from inside scripts (command files). */
       }
 
